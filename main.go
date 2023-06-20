eb6d0b6748c697369c01710cfb6e517ed2b46637
Vivek Shah <vivek.shah@outreach.io>
2023-06-20 16:02:43 -0700

    fixed issued from first pull request

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+	"text/tabwriter"
 	"time"
 
 	"database/sql"
@@ -14,6 +16,65 @@ import (
 	"github.com/go-sql-driver/mysql"
 	"github.com/urfave/cli/v2"
 )
+
+var states = map[string]bool{
+	"AL": true,
+	"AK": true,
+	"AZ": true,
+	"AR": true,
+	"AS": true,
+	"CA": true,
+	"CO": true,
+	"CT": true,
+	"DE": true,
+	"DC": true,
+	"FL": true,
+	"GA": true,
+	"GU": true,
+	"HI": true,
+	"ID": true,
+	"IL": true,
+	"IN": true,
+	"IA": true,
+	"KS": true,
+	"KY": true,
+	"LA": true,
+	"ME": true,
+	"MD": true,
+	"MA": true,
+	"MI": true,
+	"MN": true,
+	"MS": true,
+	"MO": true,
+	"MT": true,
+	"NE": true,
+	"NV": true,
+	"NH": true,
+	"NJ": true,
+	"NM": true,
+	"NY": true,
+	"NC": true,
+	"ND": true,
+	"MP": true,
+	"OH": true,
+	"OK": true,
+	"OR": true,
+	"PA": true,
+	"PR": true,
+	"RI": true,
+	"SC": true,
+	"SD": true,
+	"TN": true,
+	"TX": true,
+	"VT": true,
+	"UT": true,
+	"VA": true,
+	"VI": true,
+	"WA": true,
+	"WV": true,
+	"WI": true,
+	"WY": true,
+}
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
@@ -35,73 +96,62 @@ func main() {
 		log.Fatal("failed to connect to database:", err)
 	}
 
-	rows, err := db.QueryContext(ctx, "SELECT * FROM customers")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
+	// rows, err := db.QueryContext(ctx, "SELECT * FROM customers")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer rows.Close()
 
-	for rows.Next() {
-		var id int
-		var email string
-		var state string
+	// for rows.Next() {
+	// 	var id int
+	// 	var email string
+	// 	var state string
 
-		if err := rows.Scan(&id, &email, &state); err != nil {
-			log.Fatal(err)
-		}
+	// 	if err := rows.Scan(&id, &email, &state); err != nil {
+	// 		log.Fatal(err)
+	// 	}
 
-		fmt.Println(id, email, state)
-	}
+	// 	fmt.Println(id, email, state)
+	// }
 
-	if err := rows.Err(); err != nil {
-		log.Fatal(err)
-	}
+	// if err := rows.Err(); err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	app := &cli.App{
 		Name: "store",
 		Commands: []*cli.Command{
 			{
-				Name:  "example",
-				Usage: "show a bunch of stuff",
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:  "name",
-						Usage: "the name we want to display",
-					},
-				},
-				Action: func(cCtx *cli.Context) error {
-					name := "<none>"
-					if cCtx.String("name") != "" {
-						name = cCtx.String("name")
-					}
-
-					fmt.Println("the first argument you added: ", cCtx.Args().First())
-					fmt.Println("the name flag you passed: ", name)
-					return nil
-				},
-			},
-			{
 				Name:  "create-customer",
 				Usage: "Creates a new customer to go in the customers database, must specify email and state(2 letter code)",
 				Action: func(cCtx *cli.Context) error {
-					if len(os.Args) != 4 {
+					if cCtx.NArg() < 2 {
 						return errors.New("Must specify email and state")
 					}
-					email := os.Args[2]
-					state := os.Args[3]
+					email := cCtx.Args().Get(0)
+					state := cCtx.Args().Get(1)
+					if len(state) != 2 {
+						return errors.New("State length must be 2")
+					}
+					_, ok := states[strings.ToUpper(state)]
+					if !ok {
+						return errors.New("State must be a valid U.S. State or Territory")
+					}
+
 					insertStatement := "INSERT INTO Customers (email, state) VALUES (?, ?)"
-					_, err := db.Exec(insertStatement, email, state)
+					res, err := db.Exec(insertStatement, email, state)
+
 					if err != nil {
-						log.Fatal(err)
+						return err
 					}
-					idQuery := "SELECT LAST_INSERT_ID()"
-					var customerID int
-					err = db.QueryRow(idQuery).Scan(&customerID)
+					customerID, err := res.LastInsertId()
 					if err != nil {
-						log.Fatal(err)
+						return err
 					}
-					fmt.Println("CUSTOMER_ID\tEMAIL\t\t\tSTATE")
-					fmt.Printf("%d\t\t%s\t%s\n", customerID, email, state)
+					w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+					fmt.Fprintln(w, "Customer_id\tEmail\tState\t")
+					fmt.Fprintf(w, "%d\t%s\t%s\t\n", customerID, email, state)
+					w.Flush()
 
 					return nil
 				},
@@ -121,39 +171,40 @@ func main() {
 					}
 
 					name := cCtx.Args().Get(0)
-					price, err1 := strconv.Atoi(cCtx.Args().Get(1))
-					if err1 != nil {
-						log.Fatal(err1)
+					price, err := strconv.Atoi(cCtx.Args().Get(1))
+					if err != nil {
+						return err
 					}
 					sku := cCtx.String("sku")
 					if sku != "" {
 						insertStatement := "INSERT INTO Products (name, price, sku) VALUES (?, ?, ?)"
-						_, err := db.Exec(insertStatement, name, price, sku)
+						res, err := db.Exec(insertStatement, name, price, sku)
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
-						idQuery := "SELECT LAST_INSERT_ID()"
-						var productId int
-						err = db.QueryRow(idQuery).Scan(&productId)
+
+						productId, err := res.LastInsertId()
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
-						fmt.Println("Product_ID\tName\t\t\tPrice\t\t\tSku")
-						fmt.Printf("%d\t\t%s\t%v\t%s\n", productId, name, price, sku)
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+						fmt.Fprintln(w, "Product_id\tName\tPrice\tSku\t")
+						fmt.Fprintf(w, "%d\t%s\t%v\t%v\t\n", productId, name, price, sku)
+						w.Flush()
 					} else {
 						insertStatement := "INSERT INTO Products (name, price) VALUES (?, ?)"
-						_, err := db.Exec(insertStatement, name, price)
+						res, err := db.Exec(insertStatement, name, price)
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
-						idQuery := "SELECT LAST_INSERT_ID()"
-						var productId int
-						err = db.QueryRow(idQuery).Scan(&productId)
+						productId, err := res.LastInsertId()
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
-						fmt.Println("PRODUCT_ID\tNAME\t\t\tPRICE")
-						fmt.Printf("%d\t\t%s\t%v\n", productId, name, price)
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+						fmt.Fprintln(w, "Product_id\tName\tPrice\t")
+						fmt.Fprintf(w, "%d\t%s\t%v\t\n", productId, name, price)
+						w.Flush()
 					}
 					return nil
 
@@ -166,27 +217,28 @@ func main() {
 					if len(os.Args) != 4 {
 						return errors.New("Must specify product_id and customer_id")
 					}
-					pId, error1 := strconv.Atoi(os.Args[2])
-					cId, err1 := strconv.Atoi(os.Args[3])
-					if error1 != nil {
-						log.Fatal(error1)
+					pID, err := strconv.Atoi(os.Args[2])
+					if err != nil {
+						return err
 					}
-					if err1 != nil {
-						log.Fatal(err1)
+					cID, err := strconv.Atoi(os.Args[3])
+					if err != nil {
+						return err
 					}
 					insertStatement := "INSERT INTO Orders (customer_id, product_id) VALUES (?, ?)"
-					_, err := db.Exec(insertStatement, cId, pId)
+					res, err := db.Exec(insertStatement, cID, pID)
 					if err != nil {
-						log.Fatal(err)
+						return err
 					}
-					idQuery := "SELECT LAST_INSERT_ID()"
-					var orderId int
-					err = db.QueryRow(idQuery).Scan(&orderId)
+					orderID, err := res.LastInsertId()
 					if err != nil {
-						log.Fatal(err)
+						return err
 					}
-					fmt.Println("Order_id\tProduct_id\tCustomer_id")
-					fmt.Printf("%d\t\t%v\t%v\n", orderId, pId, cId)
+
+					w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+					fmt.Fprintln(w, "Order_id\tProduct_id\tCustomer_id\t")
+					fmt.Fprintf(w, "%v\t%v\t%v\t\n", orderID, pID, cID)
+					w.Flush()
 
 					return nil
 				},
@@ -203,52 +255,27 @@ func main() {
 				Action: func(cCtx *cli.Context) error {
 					if cCtx.String("name") != "" {
 						name := cCtx.String("name")
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
 
-						fmt.Println("Product_ID\tName\t\t\tPrice\t\t\tSku")
 						rows, err := db.QueryContext(ctx, "SELECT * FROM PRODUCTS WHERE Name LIKE CONCAT('%', ?, '%')", name)
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
 						defer rows.Close()
 
-						for rows.Next() {
-							var id int
-							var name string
-							var price float64
-							var sku sql.NullString
-
-							if err := rows.Scan(&id, &name, &price, &sku); err != nil {
-								log.Fatal(err)
-							}
-							if !sku.Valid {
-								fmt.Printf("%d\t\t%v\t\t\t%v\n", id, name, price)
-							} else {
-								fmt.Printf("%d\t\t%v\t%v\t\t\t%v\n", id, name, price, sku.String)
-							}
-						}
+						productPrintHelper(rows, w)
+						w.Flush()
 					} else {
-						fmt.Println("Product_ID\tName\t\t\tPrice\t\t\tSku")
+
 						rows, err := db.QueryContext(ctx, "SELECT * FROM PRODUCTS")
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
 						defer rows.Close()
+						productPrintHelper(rows, w)
+						w.Flush()
 
-						for rows.Next() {
-							var id int
-							var name string
-							var price float64
-							var sku sql.NullString
-
-							if err := rows.Scan(&id, &name, &price, &sku); err != nil {
-								log.Fatal(err)
-							}
-							if !sku.Valid {
-								fmt.Printf("%d\t\t%v\t\t\t%v\n", id, name, price)
-							} else {
-								fmt.Printf("%d\t\t%v\t%v\t\t\t%vtn", id, name, price, sku.String)
-							}
-						}
 					}
 					return nil
 				},
@@ -271,81 +298,49 @@ func main() {
 						email := cCtx.String("email")
 						state := cCtx.String("state")
 
-						fmt.Println("Customer_ID\tEmail\t\t\tState")
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
 						rows, err := db.QueryContext(ctx, "SELECT * FROM Customers WHERE Email LIKE CONCAT('%', ?, '%') AND State LIKE CONCAT('%', ?, '%')", email, state)
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
 						defer rows.Close()
 
-						for rows.Next() {
-							var id int
-							var email string
-							var state string
-							if err := rows.Scan(&id, &email, &state); err != nil {
-								log.Fatal(err)
-							}
-							fmt.Printf("%d\t\t%v\t%v\n", id, email, state)
+						customersPrintHelper(rows, w)
+						w.Flush()
 
-						}
 					} else if cCtx.String("email") != "" {
 						email := cCtx.String("email")
-						fmt.Println("Customer_ID\tEmail\t\t\tState")
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
 						rows, err := db.QueryContext(ctx, "SELECT * FROM Customers WHERE Email LIKE CONCAT('%', ?, '%')", email)
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
 						defer rows.Close()
 
-						for rows.Next() {
-							var id int
-							var email string
-							var state string
-							if err := rows.Scan(&id, &email, &state); err != nil {
-								log.Fatal(err)
-							}
-							fmt.Printf("%d\t\t%v\t%v\n", id, email, state)
-
-						}
+						customersPrintHelper(rows, w)
+						w.Flush()
 					} else if cCtx.String("state") != "" {
 						state := cCtx.String("state")
-						fmt.Println("Customer_ID\tEmail\t\t\tState")
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
 						rows, err := db.QueryContext(ctx, "SELECT * FROM Customers WHERE State LIKE CONCAT('%', ?, '%')", state)
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
 						defer rows.Close()
 
-						for rows.Next() {
-							var id int
-							var email string
-							var state string
-							if err := rows.Scan(&id, &email, &state); err != nil {
-								log.Fatal(err)
-							}
-							fmt.Printf("%d\t\t%v\t%v\n", id, email, state)
-
-						}
+						customersPrintHelper(rows, w)
+						w.Flush()
 					} else {
 
-						fmt.Println("Customer_ID\tEmail\t\t\tState")
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
 						rows, err := db.QueryContext(ctx, "SELECT * FROM Customers")
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
 						defer rows.Close()
 
-						for rows.Next() {
-							var id int
-							var email string
-							var state string
-
-							if err := rows.Scan(&id, &email, &state); err != nil {
-								log.Fatal(err)
-							}
-							fmt.Printf("%d\t\t%v\t%v\n", id, email, state)
-
-						}
+						customersPrintHelper(rows, w)
+						w.Flush()
 					}
 					return nil
 				},
@@ -354,112 +349,63 @@ func main() {
 				Name:  "show-orders",
 				Usage: "displays all the orders within the orders database with an optional customerId and productId filter",
 				Flags: []cli.Flag{
-					&cli.StringFlag{
+					&cli.IntFlag{
 						Name:  "customer-id",
 						Usage: "the customer-id of the customer",
 					},
-					&cli.StringFlag{
+					&cli.IntFlag{
 						Name:  "product-id",
 						Usage: "the product-id of the product",
 					},
 				}, Action: func(cCtx *cli.Context) error {
-					if cCtx.String("customer-id") != "" && cCtx.String("product-id") != "" {
-						cId := cCtx.String("customer-id")
-						pId := cCtx.String("product-id")
-
-						fmt.Println("Order_ID\tCustomer_ID\t\t\tProduct_ID")
-						rows, err := db.QueryContext(ctx, "SELECT * FROM ORDERS WHERE customer_id LIKE CONCAT('%', ?, '%') AND product_id LIKE CONCAT('%', ?, '%')", cId, pId)
+					if cCtx.Int("customer-id") != 0 && cCtx.Int("product-id") != 0 {
+						cId := cCtx.Int("customer-id")
+						pId := cCtx.Int("product-id")
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+						rows, err := db.QueryContext(ctx, "SELECT * FROM ORDERS WHERE customer_id=? AND product_id=?", cId, pId)
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
 						defer rows.Close()
 
-						for rows.Next() {
-							var id int
-							var date sql.NullString
-							var cId string
-							var pId string
-							var sku sql.NullString
+						printHelper(rows, w)
+						w.Flush()
+					} else if cCtx.Int("customer-id") != 0 {
+						cId := cCtx.Int("customer-id")
 
-							if err := rows.Scan(&id, &date, &cId, &pId, &sku); err != nil {
-								log.Fatal(err)
-							}
-
-							fmt.Printf("%d\t\t%v\t\t\t%v\n", id, cId, pId)
-
-						}
-					} else if cCtx.String("customer-id") != "" {
-						cId := cCtx.String("customer-id")
-
-						fmt.Println("Order_ID\tCustomer_ID\t\t\tProduct_ID")
-						rows, err := db.QueryContext(ctx, "SELECT * FROM ORDERS WHERE customer_id LIKE CONCAT('%', ?, '%')", cId)
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+						rows, err := db.QueryContext(ctx, "SELECT * FROM ORDERS WHERE customer_id =?", cId)
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
 						defer rows.Close()
 
-						for rows.Next() {
-							var id int
-							var date sql.NullString
-							var cId string
-							var pId string
-							var sku sql.NullString
+						printHelper(rows, w)
+						w.Flush()
+					} else if cCtx.Int("product-id") != 0 {
 
-							if err := rows.Scan(&id, &date, &cId, &pId, &sku); err != nil {
-								log.Fatal(err)
-							}
+						pId := cCtx.Int("product-id")
 
-							fmt.Printf("%d\t\t%v\t\t\t%v\n", id, cId, pId)
-
-						}
-					} else if cCtx.String("product-id") != "" {
-
-						pId := cCtx.String("product-id")
-
-						fmt.Println("Order_ID\tCustomer_ID\t\t\tProduct_ID")
-						rows, err := db.QueryContext(ctx, "SELECT * FROM ORDERS WHERE product_id LIKE CONCAT('%', ?, '%')", pId)
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+						rows, err := db.QueryContext(ctx, "SELECT * FROM ORDERS WHERE product_id =?", pId)
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
 						defer rows.Close()
 
-						for rows.Next() {
-							var id int
-							var date sql.NullString
-							var cId string
-							var pId string
-							var sku sql.NullString
-
-							if err := rows.Scan(&id, &date, &cId, &pId, &sku); err != nil {
-								log.Fatal(err)
-							}
-
-							fmt.Printf("%d\t\t%v\t\t\t%v\n", id, cId, pId)
-
-						}
+						printHelper(rows, w)
+						w.Flush()
 					} else {
 
-						fmt.Println("Order_ID\tCustomer_ID\t\t\tProduct_ID")
+						w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
 						rows, err := db.QueryContext(ctx, "SELECT * FROM ORDERS ")
 						if err != nil {
-							log.Fatal(err)
+							return err
 						}
 						defer rows.Close()
 
-						for rows.Next() {
-							var id int
-							var date sql.NullString
-							var cId string
-							var pId string
-							var sku sql.NullString
-
-							if err := rows.Scan(&id, &date, &cId, &pId, &sku); err != nil {
-								log.Fatal(err)
-							}
-
-							fmt.Printf("%d\t\t%v\t\t\t%v\n", id, cId, pId)
-
-						}
+						printHelper(rows, w)
+						w.Flush()
 					}
 					return nil
 				},
@@ -470,4 +416,70 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func printHelper(rows *sql.Rows, w *tabwriter.Writer) error {
+	fmt.Fprintln(w, "Order_ID\tCustomer_ID\tProduct_ID\t")
+	for rows.Next() {
+		var id int
+		var date sql.NullString
+		var cID int
+		var pID int
+		var sku sql.NullString
+
+		if err := rows.Scan(&id, &date, &cID, &pID, &sku); err != nil {
+			return err
+		}
+
+		fmt.Fprintf(w, "%d\t%d\t%d\t\n", id, cID, pID)
+
+	}
+	if rows.Err() != nil {
+		return rows.Err()
+	}
+	return nil
+}
+
+func customersPrintHelper(rows *sql.Rows, w *tabwriter.Writer) error {
+	fmt.Fprintln(w, "Customer_ID\tEmail\tState")
+	for rows.Next() {
+		var id int
+		var email string
+		var state string
+		if err := rows.Scan(&id, &email, &state); err != nil {
+			return err
+		}
+		fmt.Fprintf(w, "%d\t%v\t%v\n", id, email, state)
+
+	}
+	if rows.Err() != nil {
+		return rows.Err()
+	}
+	return nil
+}
+
+func productPrintHelper(rows *sql.Rows, w *tabwriter.Writer) error {
+	fmt.Fprintln(w, "Product_id\tName\tPrice\tSku\t")
+	for rows.Next() {
+		var id int
+		var name string
+		var price float64
+		var sku sql.NullString
+
+		if err := rows.Scan(&id, &name, &price, &sku); err != nil {
+			return err
+		}
+		if !sku.Valid {
+
+			fmt.Fprintf(w, "%d\t%v\t%v\t\n", id, name, price)
+		} else {
+			fmt.Fprintf(w, "%d\t%v\t%v\t%v\n", id, name, price, sku.String)
+
+		}
+	}
+	if rows.Err() != nil {
+		return rows.Err()
+	}
+	return nil
 }
